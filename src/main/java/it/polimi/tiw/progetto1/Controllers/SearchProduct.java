1ceb9c993c280ec2ebd1764f8dcf8ab5e967a794
Matteo <matteo3.@scaccabarozzi@mail.polimi.it>
2021-08-11 10:23:19 +0200

    SearchProduct.java

--- src/main/java/it/polimi/tiw/progetto1/Controllers/SearchProduct.java
+++ src/main/java/it/polimi/tiw/progetto1/Controllers/SearchProduct.java
@@ -4,9 +4,33 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @WebServlet(name = "SearchProduct", value = "/SearchProduct")
 public class SearchProduct extends HttpServlet {
+
+    private Connection connection = null;
+
+    public void init() throws ServletException {
+
+        try {
+            ServletContext context = getServletContext();
+            String driver = context.getInitParameter("dbDriver");
+            String url = context.getInitParameter("dbUrl");
+            String user = context.getInitParameter("dbUser");
+            String password = context.getInitParameter("dbPassword");
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+
+        } catch (ClassNotFoundException e) {
+            throw new UnavailableException("Impossibile caricare dbDriver");
+        } catch (SQLException e) {
+            throw new UnavailableException("Impossibile connettersi");
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
