82223f4b7439d9283b45e199dd607731229e5a8d
Matteo <matteo3.@scaccabarozzi@mail.polimi.it>
2021-08-28 12:06:53 +0200

    Add date Order

--- src/main/java/it/polimi/tiw/progetto1/Beans/Order.java
+++ src/main/java/it/polimi/tiw/progetto1/Beans/Order.java
@@ -2,6 +2,7 @@ package it.polimi.tiw.progetto1.Beans;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Order implements Serializable {
@@ -14,6 +15,7 @@ public class Order implements Serializable {
     private float shipmentFees;
     private float total;
     private int numOrder;
+    private Date date;
 
     public int getNumOrder() {
         return numOrder;
@@ -23,11 +25,12 @@ public class Order implements Serializable {
         this.numOrder = numOrder;
     }
 
-    public Order (String supplierCode, float shipmentFees, float total, int numOrder) {
+    public Order (String supplierCode, float shipmentFees, float total, int numOrder, Date date) {
         this.supplierCode = supplierCode;
         this.shipmentFees = shipmentFees;
         this.total = total;
         this.numOrder = numOrder;
+        this.date = date;
     }
 
     public float getTotal() {
@@ -81,5 +84,9 @@ public class Order implements Serializable {
 
     public String getSupplierName() {
         return supplierName;
+    }
+
+    public Date getDate() {
+        return date;
     }
 }
